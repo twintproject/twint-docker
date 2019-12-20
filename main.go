6103b480264f3833e54bcd0dec4ac894065719d6
x0rzkov <x0rzkov@gmail.com>
2019-12-20 19:27:01 +0100

    remove hard coded templates from main.go

--- main.go
+++ main.go
@@ -206,7 +206,8 @@ func main() {
 	generateTravis(vcsTags)
 }
 
-func loadConfig(path ...string) (*Config, error) {
+func loadConfig(paths ...string) (*Config, error) {
+	// load config from paths
 	err := configor.New(&configor.Config{
 		Debug:                cfg.DebugMode,
 		Verbose:              verboseMode,
@@ -216,7 +217,7 @@ func loadConfig(path ...string) (*Config, error) {
 		AutoReloadCallback: func(config interface{}) {
 			fmt.Printf("%v changed", config)
 		},
-	}).Load(cfg, path...)
+	}).Load(cfg, paths...)
 	return cfg, err
 }
 
@@ -258,12 +259,10 @@ type travisData struct {
 }
 
 func generateTravis(vcsTag []*vcsTag) error {
-
 	tmpl, err := Asset(".travis.yml")
 	if err != nil {
 		return err
 	}
-
 	tTravisfile := template.Must(template.New("tmplTravis").Parse(string(tmpl)))
 	travisfile, err := os.Create(".travis.yml")
 	if err != nil {
@@ -520,245 +519,3 @@ func getRemoteTags() (error, []string) {
 	}
 	return nil, tags
 }
-
-const (
-	entrypointTemplate = `#!/bin/bash
-$@`
-)
-
-const (
-	alpineTemplate = `FROM alpine:3.10 AS build
-
-WORKDIR /opt/app
-
-# Install Python and external dependencies, including headers and GCC
-RUN apk add --no-cache python3 python3-dev py3-pip libffi libffi-dev musl-dev gcc git ca-certificates openblas-dev musl-dev g++
-
-# Install Pipenv
-RUN pip3 install pipenv
-
-# Create a virtual environment and activate it
-RUN python3 -m venv /opt/venv
-ENV PATH="/opt/venv/bin:$PATH" \
-	VIRTUAL_ENV="/opt/venv"
-
-# Install dependencies into the virtual environment with Pipenv
-RUN git clone --depth=1 -b {{.Version}} https://github.com/twintproject/twint /opt/app \
-	&& cd /opt/app \
-	&& pip3 install --upgrade pip \
-	&& pip3 install cython \
-	&& pip3 install numpy \
-	&& pip3 install .
-
-FROM alpine:3.10
-MAINTAINER x0rxkov <x0rxkov@protonmail.com>
-
-# Create runtime user
-RUN mkdir -p /opt \
-	&& adduser -D twint -h /opt/app -s /bin/sh \
- 	&& su twint -c 'cd /opt/app; mkdir -p data'
-
-# Install Python and external runtime dependencies only
-RUN apk add --no-cache python3 libffi openblas libstdc++
-
-# Switch to user context
-USER twint
-WORKDIR /opt/app
-
-# Copy the virtual environment from the previous image
-COPY --from=build /opt/venv /opt/venv
-
-# Activate the virtual environment
-ENV PATH="/opt/venv/bin:$PATH" \
-	VIRTUAL_ENV="/opt/venv"
-
-ENTRYPOINT ["twint"]`
-)
-
-const (
-	debianSlimTemplate = `FROM python:3.7-slim-stretch
-
-MAINTAINER x0rxkov <x0rxkov@protonmail.com>
-
-ARG TWINT_VERSION={{.Version}}
-
-COPY docker-entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-
-RUN \
-apt-get update && \
-apt-get install -y \
-git
-
-RUN \
-pip3 install --upgrade -e git+https://github.com/twintproject/twint.git@{{.Version}}#egg=twint
-
-RUN \
-apt-get clean autoclean && \
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-ENTRYPOINT ["/entrypoint.sh"]
-VOLUME /twint
-WORKDIR /srv/twint`
-)
-
-const (
-	ubuntuTemplate = `FROM ubuntu:19.10
-
-MAINTAINER Sébastien Houzet (yoozio.com) <sebastien@yoozio.com>
-
-ARG TWINT_VERSION={{.Version}}
-
-COPY docker-entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-
-RUN \
-apt-get update && \
-apt-get install -y \
-git \
-python3-pip
-
-RUN \
-pip3 install --upgrade -e git+https://github.com/twintproject/twint.git@{{.Version}}#egg=twint
-
-RUN \
-apt-get clean autoclean && \
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-
-ENTRYPOINT ["/entrypoint.sh"]
-VOLUME /twint
-WORKDIR /srv/twint`
-)
-
-const (
-	travisTemplate = `after_script:
-  - docker images
-
-sudo: required
-
-before_install:
-  - sudo rm -f /usr/local/bin/docker-slim
-  - sudo rm -f /usr/local/bin/docker-slim-sensor
-  - curl -L https://github.com/docker-slim/docker-slim/releases/download/1.26.1/dist_linux.tar.gz --output docker-slim.tar.gz
-  - tar xvf docker-slim.tar.gz
-  - chmod +x dist_linux/docker-slim
-  - chmod +x dist_linux/docker-slim-sensor
-  - sudo mv dist_linux/docker-slim /usr/local/bin
-  - sudo mv dist_linux/docker-slim-sensor /usr/local/bin
-  - echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
-  - sudo service docker restart
-
-before_script:
-  - cd dockerfiles/"$VERSION"
-  - IMAGE="x0rzkov/twint:${VERSION/\//-}"
-
-env:{{range $val := .Versions}}
-  - VERSION={{ $val.Dir }}
-  - VERSION={{ $val.Dir }}/alpine
-  - VERSION={{ $val.Dir }}/slim{{end}}
-
-language: bash
-
-script:
-  - docker-slim version
-  - docker build --squash -t "$IMAGE" .
-  - sudo docker-slim build "$IMAGE"
-  - docker images
-  - docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-  - docker push "$IMAGE"
-
-services: docker`
-)
-
-const (
-	dockerComposeTemplate = `---
-version: '3.7'
-services:
-
-  twint:
-    build:
-      context: dockerfiles/latest/alpine
-      dockerfile: Dockerfile
-    container_name: twint
-  
-  elasticsearch:
-    image: docker.elastic.co/elasticsearch/elasticsearch:${KIBANA_VERSION}
-    container_name: twint_elastic
-    environment:
-    - node.name=elasticsearch
-    - cluster.initial_master_nodes=elasticsearch
-    - cluster.name=docker-cluster
-    - bootstrap.memory_lock=true
-    - "ES_JAVA_OPTS=${ELASTIC_JAVA_OPTS}"
-    ulimits:
-      memlock:
-        soft: -1
-        hard: -1
-    volumes:
-    - esdata01:/usr/share/elasticsearch/data
-    ports:
-    - 9200:9200
-
-  kibana:
-    image: docker.elastic.co/kibana/kibana:${KIBANA_VERSION}
-    container_name: twint_kibana
-    ports:
-    - 5601:5601
-
-volumes:
-  esdata01:
-    driver: local
-
-networks:
-  default:
-    external:
-      name: nw_twint`
-)
-
-const (
-	dockerignoreTemplate = `slim
-alpine
-Makefile
-docker-compose.yml
-docker-compose.*.yml
-.git
-.git/
-.git/*
-.git/**
-`
-)
-
-const (
-	makefileTemplate = `IMAGE := x0rzkov/twint-docker
-VERSION:= $(shell grep TWINT_VERSION Dockerfile | awk '{print $2}' | cut -d '=' -f 2)
-
-## test		:	test.
-test:
-	true
-
-## version	:	display version.
-version:
-	@echo $(VERSION)
-
-## image		:	build image and tag them.
-.PHONY: image
-image:
-	@docker build -t ${IMAGE}:${VERSION} .
-	@docker tag ${IMAGE}:${VERSION} ${IMAGE}:latest
-
-## push-image	:	push docker image.
-.PHONY: push-image
-push-image:
-	@docker push ${IMAGE}:${VERSION}
-	@docker push ${IMAGE}:latest
-
-## help		:	Print commands help.
-.PHONY: help
-help : Makefile
-	@sed -n 's/^##//p' $<
-
-# https://stackoverflow.com/a/6273809/1826109
-%:
-	@:
-`
-)
